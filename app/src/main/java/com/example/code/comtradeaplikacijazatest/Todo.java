b8aad75f60fb3259939e8a813c5b136de4482736
Marko Djurdjev <marko.djurdjev93@gmail.com>
2018-01-24 18:28:39 +0100

    dodao novu klasu i layout

---
+++ app/src/main/java/com/example/code/comtradeaplikacijazatest/Todo.java
@@ -0,0 +1,18 @@
+package com.example.code.comtradeaplikacijazatest;
+
+public class Todo {
+
+    private String title;
+
+    public Todo(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
